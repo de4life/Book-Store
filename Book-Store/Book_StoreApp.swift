8565ab330f9955945239c77ee38a6f0ba05b9d42
de4life <ageev_artur@ukr.net>
2023-07-02 23:41:46 +0300

    Initial Commit

---
+++ Book-Store/Book_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_StoreApp.swift
+//  Book-Store
+//
+//  Created by Артур Агеев on 02.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Book_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
