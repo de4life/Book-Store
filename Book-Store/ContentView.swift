2ad7a26197a9273ce4bbe1dc26016f8432808481
de4life <ageev_artur@ukr.net>
2023-07-10 08:51:58 +0300

    add book row

--- Book-Store/ContentView.swift
+++ Book-Store/ContentView.swift
@@ -1,21 +1,21 @@
-//
-//  ContentView.swift
-//  Book-Store
-//
-//  Created by Артур Агеев on 02.07.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel = BookViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(viewModel.books, id: \.title) { book in
+                NavigationLink(destination: BookDetailView(book: book)) {
+                    BookRowView(book: book)
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("Bookstore")
+            .onAppear {
+                viewModel.fetchBooks()
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +24,37 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct BookRowView: View {
+    let book: Book
+    
+    var body: some View {
+        HStack {
+            AsyncImage(url: book.bookImageURL) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 120)
+            } placeholder: {
+                Color.gray
+                    .frame(width: 80, height: 120)
+            }
+            
+            VStack(alignment: .leading) {
+                Text(book.title)
+                    .font(.headline)
+                Text(book.publicationDate)
+                    .font(.subheadline)
+            }
+        }
+    }
+}
+
+struct BookRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookRowView(book: Book(title: "Book Title", publicationDate: "2023-01-01", bookImageURL: URL(string: "https://example.com/image.jpg")!))
+            .previewLayout(.fixed(width: 300, height: 100))
+            .padding()
+    }
+}
+
